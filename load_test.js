78d56e37d6e7fbd8f0fa47844aabff7673052986
Milos Pantic <milospantic96@hotmail.com>
2022-05-18 18:08:11 +0200

    Update load_test.js
    remove secrets

--- load_test.js
+++ load_test.js
@@ -3,8 +3,8 @@ import http from 'k6/http';
 export default function () {
   const url = 'http://localhost:3000';
   const payload = JSON.stringify({
-    username: 'ravi.van.test@gmail.com',
-    password: 'ravi.van.test@gmail.com',
+    username: '',
+    password: '',
   });
 
   let options = {
